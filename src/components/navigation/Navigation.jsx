9febb28a8ea4c55e980cf79f3ee31b812a646f6c
ChrisPlease <chris@chrisplease.me>
2018-07-09 16:15:07 -0600

    init css-modules config

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -3,6 +3,11 @@ import { NavLink } from 'react-router-dom';
 
 import logo from '../../assets/img/logo.svg';
 
+<<<<<<< HEAD
+=======
+import styles from './Navigation.css';
+
+>>>>>>> b4ad5ab... init css-modules config
 const shortenedNav = (exact = false, to, name) => {
   return <NavLink exact={exact} activeClassName="active" to={to}>{name}</NavLink>;
 };
@@ -10,10 +15,18 @@ const shortenedNav = (exact = false, to, name) => {
 function Navigation () {
   return (
     <Fragment>
+<<<<<<< HEAD
       <nav role="navigation">
+=======
+      <nav className={styles.navigation} role="navigation">
+>>>>>>> b4ad5ab... init css-modules config
         {shortenedNav(true, '/', 'Home')}
+        <img src={logo} alt="Native logo"/>
       </nav>
+<<<<<<< HEAD
       <img src={logo} alt="Native logo"/>
+=======
+>>>>>>> b4ad5ab... init css-modules config
     </Fragment>
   );
 }
