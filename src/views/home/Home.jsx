fe9f323d1824edf5ae5020fb096ad24628b6fe8f
ChrisPlease <chris@chrisplease.me>
2018-07-10 11:21:01 -0600

    update readme

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import { getTribes } from '../../actions/tribeActions';
+import { getTribes } from '../../actions/tribesActions';
 
 import CardList from '../../components/shared/card-list/CardList';
 
