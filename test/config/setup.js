c046acd73b6312d083053c01bbaef4260118313b
ChrisPlease <chris@chrisplease.me>
2018-07-11 15:15:05 -0600

    configure global buildComponent helper function

--- test/config/setup.js
+++ test/config/setup.js
@@ -1,8 +1,16 @@
+import React from 'react';
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+const buildComponent = (Component, props, render = shallow) => {
+  return render(<Component {...props} />);
+};
+
 global.shallow = shallow;
 global.render = render;
 global.mount = mount;
+
+global.buildComponent = buildComponent;
+
