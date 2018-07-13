cc07ccc0bc9071ff07acb9bdf034aeb3fe04ac7e
ChrisPlease <chris@chrisplease.me>
2018-07-13 14:00:55 -0600

    add hot reloading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import { hot } from 'react-hot-loader';
 
 import Header from './header/Header';
 import { routes } from '../routes';
@@ -14,4 +15,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default hot(module)(App);
