49b16c74eda05727f4510465d4d598a5d7a2df7d
ChrisPlease <chris@chrisplease.me>
2018-07-19 14:17:57 -0600

    restyle card component

--- src/components/shared/card/Card.jsx
+++ src/components/shared/card/Card.jsx
@@ -9,20 +9,32 @@ function Card ({tribe}) {
 
   return (
     <div className={styles.Card}>
-      <h3 className={styles.Title}>
-        <Link to={`/tribe/${tribe.id}`}>{tribe.name}</Link>
-      </h3>
-      <h4 className={styles.Subtitle}>{tribe.subtitle}</h4>
-      <img src={`/${tribe.image}`} alt=""/>
-      <p className={styles.Intro}>
-        {tribe.tribeIntro}
-      </p>
-      {tribe.tags &&
-        <ul className={styles.Taglist}>
-          {tribe.tags.map((tag, i) => <li key={i}><Tag name={tag} /></li>)}
-        </ul>
-      }
-      {tribe.location}
+      <div className={styles.Card_Container}>
+        <div className={styles.Header}>
+          <h2 className={styles.Title}>
+            <Link to={`/tribe/${tribe.id}`}>{tribe.name}</Link>
+          </h2>
+          <h3 className={styles.Subtitle}>{tribe.subtitle}</h3>
+        </div>
+        <div className={styles.Summary}>
+          <h4>About</h4>
+          <p className={styles.Intro}>
+            {tribe.tribeIntro}
+          </p>
+          {tribe.tags &&
+            <ul className={styles.Taglist}>
+              {tribe.tags.map((tag, i) => <li key={i}><Tag name={tag} /></li>)}
+            </ul>
+          }
+          Location: {tribe.location}
+        </div>
+        <div className={styles.Data}>
+          <img src={`/${tribe.image}`} alt=""/>
+        </div>
+        <div className={styles.Membership}>
+          <h4>Membership</h4>
+        </div>
+      </div>
     </div>
   );
 }
