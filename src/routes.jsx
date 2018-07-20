24ab4bcff4c675202a4f1d5c1b83cb0b83032e50
ChrisPlease <chris@chrisplease.me>
2018-07-20 10:34:23 -0600

    define routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,6 +4,8 @@ import { Route, Switch } from 'react-router';
 import Home from './views/home';
 import Tribes from './views/tribes';
 import Tribe from './views/tribe';
+import Tokens from './views/tokens';
+import FAQ from './views/faq';
 import Project from './views/project';
 import FourOhFour from './views/404';
 
@@ -12,6 +14,8 @@ export const routes = () => {
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/tribes" component={Tribes} />
+      <Route path="/tokens" component={Tokens} />
+      <Route path="/learn" component={FAQ} />
       <Route exact path="/tribe/:tribeId" component={Tribe} />
       <Route path="/tribe/:tribeId/project/:projectId" component={Project} />
       <Route component={FourOhFour} />
