cf237aa474e77b19d8ba12a19027eddf8393f814
ChrisPlease <chris@chrisplease.me>
2018-07-20 14:47:04 -0600

    dispatch loading action

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,9 +1,11 @@
 import { userActions as actions } from './actionTypes';
+import { beginAjaxCall } from './loadingActions';
 import { getAccount } from '../web3';
 
 export const getUserAccount = () => {
   return async dispatch => {
     dispatch({ type: actions.GET_USER_ACCOUNT });
+    dispatch(beginAjaxCall());
     try {
       const data = await getAccount();
       return dispatch(getUserAccountSuccess(data));
