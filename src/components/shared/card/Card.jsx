c1e84fcb222fbdad912848c13a84e5c3db58a4f0
ChrisPlease <chris@chrisplease.me>
2018-07-20 10:36:07 -0600

    add render prop to the card component to handle single view

--- src/components/shared/card/Card.jsx
+++ src/components/shared/card/Card.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Button from '../button';
 import Tag from '../tag';
 
 import styles from './Card.css';
 
-function Card ({tribe}) {
+function Card ({tribe, render}) {
+  console.log(tribe);
+
+  const displayTitle = (bool) => {
+    if (bool) {
+      return (
+        <h2 className={styles.Title}>
+          <Link to={`/tribe/${tribe.id}`}>{tribe.name}</Link>
+        </h2>
+      );
+    } else {
+      return (
+        <h2 className={styles.Title}>{tribe.name}</h2>
+      );
+    }
+  }
 
   return (
     <div className={styles.Card}>
       <div className={styles.Card_Container}>
         <div className={styles.Header}>
-          <h2 className={styles.Title}>
-            <Link to={`/tribe/${tribe.id}`}>{tribe.name}</Link>
-          </h2>
+          {displayTitle(!render)}
           <h3 className={styles.Subtitle}>{tribe.subtitle}</h3>
         </div>
         <div className={styles.Summary}>
@@ -30,11 +44,15 @@ function Card ({tribe}) {
         </div>
         <div className={styles.Data}>
           <img src={`/${tribe.image}`} alt=""/>
+          TBD: Data
         </div>
         <div className={styles.Membership}>
           <h4>Membership</h4>
+          <p>{tribe.tribePurpose}</p>
+          <Button centered theme="secondary" content={`Support ${tribe.name}`} />
         </div>
       </div>
+      {render && render()}
     </div>
   );
 }
