882866b4f51b15c7a387f69b77946cef5d31dd2a
ChrisPlease <chris@chrisplease.me>
2018-07-21 15:56:30 -0600

    update header to new designs and implement modal

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,41 +1,80 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Button from '../button';
 import Navigation from '../navigation';
+import Modal from '../modal';
 
 import logo from '../../../assets/img/logo.svg';
 import metamask from '../../../assets/img/metamask.svg';
 
 import styles from './Header.css';
 
-function Header () {
-  return (
-    <header>
-      <div className={styles.Header}>
-        <Link className={styles.LogoLink} to="/">
-          <img className={styles.Logo} src={logo} alt="Native logo"/>
-        </Link>
-        <Navigation />
-        <div className={styles.Metamask}>
-          <img src={metamask} alt="Metamask Connected"/>
-          <svg width="10px" height="10px" viewBox="0 0 10 10">
-            <circle r="5" cx="5" cy="5" fill="#2C5E5E" />
-          </svg>
-        </div>
-      </div>
-      <div className={styles.Subheader}>
-        <div className="container">
+export class Header extends Component {
+
+  state = {
+    isModalOpen: false
+  }
+
+  openModal() {
+    this.setState({isModalOpen: true});
+  }
+
+  closeModal() {
+    this.setState({isModalOpen: false});
+  }
+
+  render() {
+    return (
+      <header>
+        <div className={styles.Header}>
+          <Link className={styles.LogoLink} to="/">
+            <img className={styles.Logo} src={logo} alt="Native logo"/>
+          </Link>
           <Button
               rounded
-              theme="tertiary"
-              clickHandler={() => alert('Clicked!')}
-              content="Get Native Tokens"
+              outline
+              theme="primary"
+              clickHandler={this.openModal.bind(this)}
+              content="Get NT"
           />
+          <Navigation />
+          <div className={styles.Metamask}>
+            <img src={metamask} alt="Metamask Connected"/>
+            <svg width="10px" height="10px" viewBox="0 0 10 10">
+              <circle r="5" cx="5" cy="5" fill="#2C5E5E" />
+            </svg>
+          </div>
         </div>
-      </div>
-    </header>
-  );
+        <div className={styles.Subheader}>
+          <div className="container">
+          </div>
+        </div>
+        <Modal
+            title="Lorem Ipsum"
+            isModalOpen={this.state.isModalOpen}
+            closeModal={this.closeModal.bind(this)}
+            render={() => {
+              return (
+                <div>Foo
+                  <Button
+                    centered
+                    theme="secondary"
+                    clickHandler={this.closeModal.bind(this)}
+                    content="Close"
+                  />
+                </div>
+              );
+            }}
+        />
+      </header>
+    );
+  }
 }
 
-export default Header;
+export default connect(
+  null,
+  null
+)
+(Header);
