f37da724798122bad8bc77a890d6aca2353235c3
ChrisPlease <chris@chrisplease.me>
2018-07-21 17:23:44 -0600

    remove project view and route

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,18 +6,16 @@ import Tribes from './views/tribes';
 import Tribe from './views/tribe';
 import Tokens from './views/tokens';
 import FAQ from './views/faq';
-import Project from './views/project';
 import FourOhFour from './views/404';
 
 export const routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route path="/tribes" component={Tribes} />
-      <Route path="/tokens" component={Tokens} />
-      <Route path="/learn" component={FAQ} />
+      <Route exact path="/tribes" component={Tribes} />
       <Route exact path="/tribe/:tribeId" component={Tribe} />
-      <Route path="/tribe/:tribeId/project/:projectId" component={Project} />
+      <Route exact path="/tokens" component={Tokens} />
+      <Route exact path="/learn" component={FAQ} />
       <Route component={FourOhFour} />
     </Switch>
   );
