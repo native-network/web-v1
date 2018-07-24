6d65bd518b1198df62f1c49ac6f6778b91292473
Nick Arellano <nick@ideasbynature.com>
2018-07-24 09:36:24 -0600

    Move user to app container and load/set state on mount

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
 import React, { Component, Fragment } from 'react';
+import Header from './shared/header';
 import { hot } from 'react-hot-loader';
 import { connect } from 'react-redux';
-
-import Header from './shared/header';
 import { routes } from '../routes';
+import { getUserAddress } from "../actions/userActions";
 
 export class App extends Component {
+   componentDidMount = () => {
+       this.props.getUserAddress();
+  };
+
   render() {
+      const { user } = this.props;
     return (
       <Fragment>
+          <h3>{user.address}</h3>
         <Header location={this.props.location} />
         {routes()}
       </Fragment>
@@ -16,13 +22,20 @@ export class App extends Component {
   }
 }
 
+function mapDispatchToProps(dispatch) {
+    return {
+        getUserAddress: () => dispatch(getUserAddress())
+    };
+}
+
 App = connect(
   (state) => {
     return {
-      location: state.router.location
+      location: state.router.location,
+      user: state.user
     };
   },
-  null
+  mapDispatchToProps
 )(App);
 
 export default hot(module)(App);
