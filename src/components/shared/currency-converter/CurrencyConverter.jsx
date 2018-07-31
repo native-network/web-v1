cac8adbfa943359a0c833e523de04c02f0634414
ChrisPlease <chris@chrisplease.me>
2018-07-31 16:51:03 -0600

    update currency-converter to use input

--- src/components/shared/currency-converter/CurrencyConverter.jsx
+++ src/components/shared/currency-converter/CurrencyConverter.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 
 import styles from './CurrencyConverter.css';
 
+import eth from '../../../assets/img/eth.svg';
+import native from '../../../assets/img/native.svg';
+
 import Button from '../button';
+import CurrencyInput from '../currency-input';
 
 const currencies = [
   {
     id: 'ETH',
-    thumb: 'http://placehold.it/50x50',
+    thumb: eth,
+  },
+  {
+    id: 'NT',
+    thumb: native,
   },
   {
     id: 'EGTT',
@@ -27,23 +35,34 @@ function CurrencyConverter() {
   return (
     <div className={styles.ConversionInputs}>
       <div className={styles.ConversionInput}>
-        <img src={currencies[0].thumb} alt="" />
-        <label htmlFor="">
-          <span>Pay with</span>
-          {currencies[0].id}
-        </label>
-        <br />
-        <input type="text" />
+        <CurrencyInput
+          currency={currencies[0]}
+          renderLabel={(currency) => (
+            <label htmlFor={currency.id}>
+              <img src={currency.thumb} alt="" />
+              <span>Pay with</span>
+              {currency.id} &#9662;
+              <ul>
+                {currencies
+                  .filter((curr) => curr.id !== currency.id)
+                  .map((curr, i) => <li key={i}>{curr.id}</li>)}
+              </ul>
+            </label>
+          )}
+        />
       </div>
       &rarr;
       <div className={styles.ConversionInput}>
-        <img src={currencies[1].thumb} alt="" />
-        <label htmlFor="">
-          <span>Receive</span>
-          {currencies[1].id}
-        </label>
-        <br />
-        <input type="text" />
+        <CurrencyInput
+          currency={currencies[1]}
+          renderLabel={(currency) => (
+            <label htmlFor={currency.id}>
+              <img src={currency.thumb} alt="" />
+              <span>Pay with</span>
+              {currency.id} &#9662;
+            </label>
+          )}
+        />
       </div>
       <Button content="&#8644; Convert" theme="secondary" />
     </div>
