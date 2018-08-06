0c90e930898c2dadd63d4c8482d18d7c62ca7125
ChrisPlease <chris@chrisplease.me>
2018-08-06 09:01:02 -0600

    unit tests for Card component

---
+++ test/components/shared/card/Card.spec.js
@@ -0,0 +1,22 @@
+import Card from '../../../../src/components/shared/card';
+
+describe.skip('Card', () => {
+  let wrapper;
+  let props;
+
+  beforeEach(() => {
+    props = {
+      tribe: {
+        id: 0,
+        name: 'Foo',
+        image: 'assets/image.jpg',
+      },
+      // render: jest.fn(),
+    };
+    wrapper = buildComponent(Card, props);
+  });
+
+  it('should render without crashing', () => {
+    expect(wrapper).toHaveLength(1);
+  });
+});
