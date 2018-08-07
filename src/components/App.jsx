a9511cd42ca7fa20ee4207fd53a9564b7923c3d0
ChrisPlease <chris@chrisplease.me>
2018-08-07 09:43:40 -0600

    update app component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,26 @@
 import React, { Component, Fragment } from 'react';
-import Header from './shared/header';
 import { hot } from 'react-hot-loader';
 import { connect } from 'react-redux';
+
+import Header from './shared/header';
+import Modal from './shared/modal';
+import WelcomeDialog from './welcome-dialog';
 import { routes } from '../routes';
+
+import styles from './App.css';
+import native from '../assets/img/native.svg';
+
 import { getUserAddress } from '../actions/userAddressActions';
 
 export class App extends Component {
+  state = {
+    isWelcomeModalOpen: true,
+  };
+
+  closeWelcomeModal() {
+    this.setState({ isWelcomeModalOpen: false });
+  }
+
   componentDidMount = () => {
     this.props.getUserAddress();
   };
@@ -13,9 +28,27 @@ export class App extends Component {
   render() {
     return (
       <Fragment>
+        <Modal
+          renderHeader={() => (
+            <div className={styles.ModalHeader}>
+              <img src={native} alt="" />
+              <h1>Welcome To Native</h1>
+              <button
+                style={{ color: 'black' }}
+                onClick={this.closeWelcomeModal.bind(this)}
+              >
+                x
+              </button>
+            </div>
+          )}
+          isOpen={this.state.isWelcomeModalOpen}
+          label="Welcome to Native"
+        >
+          <WelcomeDialog dismissDialog={this.closeWelcomeModal.bind(this)} />
+        </Modal>
         <Header
-          isLoggedIn={this.props.user.isLoggedIn}
           session={this.props.user.session}
+          isLoggedIn={this.props.isLoggedIn}
         />
         {routes()}
       </Fragment>
