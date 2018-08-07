e2569a140e8dc490fa6900c9c4c526faf3068a69
ChrisPlease <chris@chrisplease.me>
2018-08-07 16:46:12 -0600

    add new actions/reducers to post a new tribe

--- src/reducers/allTribesReducer.js
+++ src/reducers/allTribesReducer.js
@@ -1,11 +1,12 @@
-import { allTribesActions as actions } from '../actions/actionTypes';
+/* eslint-disable */
+import {allTribesActions as actions} from '../actions/actionTypes';
 
 const initialState = {
   tribes: [],
   error: '',
 };
 
-export default function allTribesReducer(state = initialState, action) {
+export default function allTribesReducer (state = initialState, action) {
   switch (action.type) {
     case actions.GET_TRIBES_SUCCESS:
       return {
@@ -17,6 +18,13 @@ export default function allTribesReducer(state = initialState, action) {
         ...initialState,
         tribes: [],
         error: action.error,
+      };
+    case actions.ADD_NEW_TRIBE_SUCCESS:
+      console.log (action);
+
+      return {
+        ...state,
+        tribes: [...state.tribes, action.tribe],
       };
     default:
       return state;
