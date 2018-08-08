020c99f6c01e9dcf76506fc1ba7c2234de20792b
ChrisPlease <chris@chrisplease.me>
2018-08-08 12:33:43 -0600

    100% coverage CurrencyInput

--- test/components/shared/currency-input/CurrencyInput.spec.js
+++ test/components/shared/currency-input/CurrencyInput.spec.js
@@ -2,12 +2,37 @@ import CurrencyInput from '../../../../src/components/shared/currency-input';
 
 describe('CurrencyInput', () => {
   let wrapper;
+  let props;
 
   beforeEach(() => {
-    wrapper = buildComponent(CurrencyInput);
+    props = {
+      currency: {
+        id: 0,
+      },
+    };
+    wrapper = buildComponent(CurrencyInput, props);
   });
 
   it('should render without crashing', () => {
     expect(wrapper).toHaveLength(1);
+  });
+
+  it('should render an input element', () => {
+    const input = wrapper.find('input');
+    expect(input).toHaveLength(1);
+  });
+
+  it('should assign `id` to `input` id', () => {
+    const input = wrapper.find('input');
+
+    expect(input.props().id).toEqual(0);
+  });
+
+  it('should call `renderLabel` when provided a `renderLabel` prop', () => {
+    const renderLabel = jest.fn();
+    props = { ...props, renderLabel };
+    wrapper = buildComponent(CurrencyInput, props);
+
+    expect(renderLabel).toHaveBeenCalled();
   });
 });
