ee88df2f02e644c1cc19b8760e39ba6ee373f962
ChrisPlease <chris@chrisplease.me>
2018-08-13 10:38:17 -0600

    responsive tribe cards

--- src/components/shared/card/Card.jsx
+++ src/components/shared/card/Card.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import Button from '../button';
 import Tag from '../tag';
@@ -7,52 +6,50 @@ import Tag from '../tag';
 import styles from './Card.css';
 
 function Card({ tribe, render }) {
-  const displayTitle = (bool) => {
-    if (bool) {
-      return (
-        <h2 className={styles.Title}>
-          <Link to={`/tribe/${tribe.id}`}>{tribe.name}</Link>
-        </h2>
-      );
-    } else {
-      return <h2 className={styles.Title}>{tribe.name}</h2>;
-    }
-  };
-
   return (
     <div className={styles.Card}>
-      <div className={styles.Card_Container}>
-        <div className={styles.Header}>
-          {displayTitle(!render)}
-          <h3 className={styles.Subtitle}>{tribe.subtitle}</h3>
+      <div
+        style={{ backgroundImage: `url("${tribe.image}")` }}
+        className={styles.Header}
+      >
+        <div className={styles.HeaderOverlay}>
+          <div className={styles.HeaderContainer}>
+            <Tag name={tribe.subtitle} />
+            <h2 className={styles.Title}>{tribe.name}</h2>
+            <span className={styles.Location}>{tribe.location}</span>
+          </div>
         </div>
-        <div className={styles.Summary}>
-          <h4>About</h4>
-          <p className={styles.Intro}>{tribe.tribeIntro}</p>
-          {tribe.tags && (
-            <ul className={styles.Taglist}>
-              {tribe.tags.map((tag, i) => (
-                <li key={i}>
-                  <Tag name={tag} />
-                </li>
-              ))}
-            </ul>
-          )}
-          Location: {tribe.location}
+      </div>
+      <div className={styles.CTAMobile}>
+        <div className={styles.CTABadge}>
+          <span> Support {tribe.name}</span>
+          <Button theme="primary" content="100 NT" />
         </div>
-        <div className={styles.Data}>
-          <img src={`/${tribe.image}`} alt="" />
-          TBD: Data
-        </div>
-        <div className={styles.Membership}>
-          <h4>Membership</h4>
-          <p>{tribe.tribePurpose}</p>
-          <Button
-            centered
-            theme="secondary"
-            content={`Support ${tribe.name}`}
-          />
-        </div>
+      </div>
+      <div className={styles.TokenDataMobile}>
+        <ul>
+          <li>Foo</li>
+          <li>Foo</li>
+          <li>Foo</li>
+          <li>Foo</li>
+        </ul>
+      </div>
+      <div className={styles.Summary}>
+        <h4>About</h4>
+        <p className={styles.Intro}>{tribe.tribeIntro}</p>
+        {tribe.tags && (
+          <ul className={styles.Taglist}>
+            {tribe.tags.map((tag, i) => (
+              <li key={i}>
+                <Tag name={tag} />
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <div className={styles.Membership}>
+        <h4>Membership</h4>
+        <p>{tribe.tribePurpose}</p>
       </div>
       {render && render()}
     </div>
