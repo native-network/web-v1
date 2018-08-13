2049ac2eab41c98a41f559ebbd9290d1edd0e20d
ChrisPlease <chris@chrisplease.me>
2018-08-13 13:11:23 -0600

    refine responsive styles

--- src/components/shared/card/Card.jsx
+++ src/components/shared/card/Card.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react';
 
 import Button from '../button';
@@ -17,6 +18,31 @@ function Card({ tribe, render }) {
             <Tag name={tribe.subtitle} />
             <h2 className={styles.Title}>{tribe.name}</h2>
             <span className={styles.Location}>{tribe.location}</span>
+
+            <div className={`${styles.TokenData} ${styles.Desktop}`}>
+              <dl>
+                <div>
+                  <dt>Token Value</dt>
+                  <dd>.031 NT ($0.09)</dd>
+                </div>
+                <div>
+                  <dt>Market Cap</dt>
+                  <dd>$312,338.25</dd>
+                </div>
+                <div>
+                  <dt>Dev Fund</dt>
+                  <dd>24,958 NT</dd>
+                </div>
+                <div>
+                  <dt>Reserve</dt>
+                  <dd>2,495.8 NT</dd>
+                </div>
+                <div>
+                  <dt>Reserve Ratio</dt>
+                  <dd>10%</dd>
+                </div>
+              </dl>
+            </div>
           </div>
         </div>
       </div>
@@ -26,13 +52,25 @@ function Card({ tribe, render }) {
           <Button theme="primary" content="100 NT" />
         </div>
       </div>
-      <div className={styles.TokenDataMobile}>
-        <ul>
-          <li>Foo</li>
-          <li>Foo</li>
-          <li>Foo</li>
-          <li>Foo</li>
-        </ul>
+      <div className={`${styles.TokenData} ${styles.Mobile}`}>
+        <dl>
+          <div>
+            <dt>Token Value</dt>
+            <dd>.031 NT ($0.09)</dd>
+          </div>
+          <div>
+            <dt>Market Cap</dt>
+            <dd>$312,338.25</dd>
+          </div>
+          <div>
+            <dt>Dev Fund</dt>
+            <dd>24,958 NT</dd>
+          </div>
+          <div>
+            <dt>Reserve</dt>
+            <dd>2,495.8 NT (10%)</dd>
+          </div>
+        </dl>
       </div>
       <div className={styles.Summary}>
         <h4>About</h4>
