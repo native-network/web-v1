c5726a048cffd5b769774d81bafbbb06ca501447
skuhlmann <samkuhlmann@gmail.com>
2018-09-05 16:22:35 -0600

    Backs out some on the fly dimension handling and fixes linting errors

--- src/components/shared/s3-image/S3Image.js
+++ src/components/shared/s3-image/S3Image.js
@@ -3,7 +3,7 @@ import { s3Path } from '../../../requests';
 
 function S3Image({ fileName, className, ...props }) {
   const filePath = s3Path(fileName);
-  return <img src={filePath} {...props} className={className} />;
+  return <img src={filePath} className={className} {...props} />;
 }
 
 export default S3Image;
