838f9fc342f83cf9c76657fe1020a0d22200f6c4
Chris Pruyne <chris@chrisplease.me>
2018-09-21 19:15:42 +0000

    Merged in HOTFIX/missing-fromwei-conversion (pull request #82)
    missing conversion
    
    Approved-by: Dave Daniel <davedan7@gmail.com>

--- src/components/dialogs/community-stake/steps/StakeCommunity.jsx
+++ src/components/dialogs/community-stake/steps/StakeCommunity.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-
+import { getWeb3ServiceInstance } from '../../../../web3/Web3Service';
+const { web3 } = getWeb3ServiceInstance();
+const { fromWei } = web3.utils;
 import { stake } from '../../../../actions/currencyActions';
-
 import Button from '../../../shared/button';
-
 import styles from '../CommunityStake.css';
 
 class StakeCommunity extends Component {
@@ -23,9 +23,11 @@ class StakeCommunity extends Component {
         <div className={styles.TextContainer}>
           <p>
             To join {community.name}, you will need to stake{' '}
-            {community.currency.minimumStake} {community.currency.symbol}. You
-            can retreive your currency if you choose to leave the community at
-            some point in the future.
+            {`${fromWei(community.currency.minimumStake)} ${
+              community.currency.symbol
+            }`}
+            . You can retreive your currency if you choose to leave the
+            community at some point in the future.
           </p>
           <p>
             <strong>
