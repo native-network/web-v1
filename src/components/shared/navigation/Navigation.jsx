f168a832b320b1d06b7d2d6a933645ec5defe64e
Chris Pruyne <chris@chrisplease.me>
2018-10-05 22:23:37 +0000

    Merged in HOTFIX/animate-primary-menu (pull request #163)
    add animation styles for primary menu
    
    Approved-by: Nick Arellano <nick@ideasbynature.com>

--- src/components/shared/navigation/Navigation.jsx
+++ src/components/shared/navigation/Navigation.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Transition } from 'react-transition-group';
 import classNames from 'classnames/bind';
 
 import Hamburger from './Hamburger';
@@ -8,10 +9,20 @@ import styles from './Navigation.css';
 
 const cx = classNames.bind(styles);
 
+const ANIMATION_DURATION = 200;
+
 class Navigation extends Component {
-  state = {
-    isMenuActive: false,
-  };
+  constructor(props) {
+    super(props);
+
+    this.menu = React.createRef();
+
+    this.state = {
+      isMenuActive: false,
+    };
+
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
 
   static defaultProps = {
     user: {
@@ -41,21 +52,53 @@ class Navigation extends Component {
       MenuOpen: this.state.isMenuActive,
     });
 
+    const transition = `all ${ANIMATION_DURATION}ms linear`;
+
+    const transitionStyles = {
+      exited: {
+        height: '0px',
+        visibility: 'hidden',
+      },
+      entered: {
+        height: `${this.menu.current && this.menu.current.offsetHeight}px`,
+      },
+      entering: {
+        height: `${this.menu.current && this.menu.current.offsetHeight}px`,
+        transition,
+      },
+      exiting: {
+        height: '0px',
+        transition,
+      },
+    };
+
     return (
       <div className={styles.Navigation}>
         <nav>
           <Hamburger
             active={this.state.isMenuActive}
-            clickHandler={this.toggleMenu.bind(this)}
+            clickHandler={this.toggleMenu}
           />
-          <Menu
-            address={address}
-            role={role}
-            hidden={!this.state.isMenuActive}
-            menuClass={classes}
-            menuItemClass={styles.MenuListItem}
-            linkClass={styles.MenuLink}
-          />
+          <Transition in={this.state.isMenuActive} timeout={ANIMATION_DURATION}>
+            {(state) => (
+              <div
+                className={styles.MenuContainer}
+                style={{
+                  ...transitionStyles[state],
+                }}
+              >
+                <Menu
+                  forwardRef={this.menu}
+                  address={address}
+                  role={role}
+                  hidden={!this.state.isMenuActive}
+                  menuClass={classes}
+                  menuItemClass={styles.MenuListItem}
+                  linkClass={styles.MenuLink}
+                />
+              </div>
+            )}
+          </Transition>
         </nav>
       </div>
     );
