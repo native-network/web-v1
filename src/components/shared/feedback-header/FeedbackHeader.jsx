4ca6ee8d23f6af3fe68dd212d887d3df9cc6e880
oovg <office@vengist.com>
2018-10-18 11:08:26 -0700

    cleans up css and adds responsive styles

--- src/components/shared/feedback-header/FeedbackHeader.jsx
+++ src/components/shared/feedback-header/FeedbackHeader.jsx
@@ -10,26 +10,26 @@ function FeedbackHeader() {
         <div className={styles.Feedback__message}>
           <p>
             <strong>Native is currently in Alpha.</strong> We want your feedback
-            so we can give you the best user experience possible.
-          </p>
-          <p>
-            Ask us anything in our
-            <a
-              className={styles.FeedbackLink}
-              href="https://telegram.me/nativeproject"
-              target="_blank"
-            >
-              Telegram
-            </a>
-            channel, or send us your bug reports and general feedback at our
-            <a
-              className={styles.FeedbackLink__zen}
-              href="https://nativeproject.zendesk.com/hc/en-us/requests/new"
-              target="_blank"
-            >
-              Zendesk
-            </a>
-            .
+            so we can give you the best user experience possible.&nbsp;
+            <span>
+              Ask us anything in our
+              <a
+                className={styles.FeedbackLink}
+                href="https://telegram.me/nativeproject"
+                target="_blank"
+              >
+                Telegram
+              </a>
+              channel, or send us your bug reports and general feedback at our
+              <a
+                className={styles.FeedbackLink__zen}
+                href="https://nativeproject.zendesk.com/hc/en-us/requests/new"
+                target="_blank"
+              >
+                Zendesk
+              </a>
+              .
+            </span>
           </p>
         </div>
       </div>
