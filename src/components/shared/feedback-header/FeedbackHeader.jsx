1dadad716107ad91574a143e35405894afb85522
skuhlmann <samkuhlmann@gmail.com>
2018-10-18 11:34:37 -0600

    Starts the feedback bar

---
+++ src/components/shared/feedback-header/FeedbackHeader.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import styles from './FeedbackHeader.css';
+
+function FeedbackHeader() {
+  return (
+    <div className={styles.FeedbackHeader__container}>
+      <div className={styles.FeedbackHeader}>
+        <span className={styles.Feedback__tag}>ALPHA</span>
+        <div className={styles.Feedback__message}>
+          <p>
+            <strong>Native is currently in Alpha.</strong> We want your feedback
+            so we can give you the best user experience possible.
+          </p>
+          <p>
+            Ask us anything in our
+            <a
+              className={styles.FeedbackLink}
+              href="https://telegram.me/nativeproject"
+              target="_blank"
+            >
+              Telegram
+            </a>
+            channel, or send us your bug reports and general feedback at our
+            <a
+              className={styles.FeedbackLink__zen}
+              href="https://nativeproject.zendesk.com/hc/en-us/requests/new"
+              target="_blank"
+            >
+              Zendesk
+            </a>
+            .
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FeedbackHeader;
