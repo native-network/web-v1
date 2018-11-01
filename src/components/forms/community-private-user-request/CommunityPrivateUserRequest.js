765fe881684da27d52844061147f3c783306a2bf
Max Sturges <>
2018-11-01 15:19:49 -0600

    added form validation

--- src/components/forms/community-private-user-request/CommunityPrivateUserRequest.js
+++ src/components/forms/community-private-user-request/CommunityPrivateUserRequest.js
@@ -1,5 +1,8 @@
 /*eslint-disable */
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Form, Field } from 'react-final-form';
+// import { Field as Field5 } from 'react-final-form-html5-validation';
+
 import styles from './CommunityPrivateUserRequest.css';
 import Button from '../../shared/button';
 
@@ -7,30 +10,98 @@ const CommunityPrivateUserRequest = ({ community, user }) => {
   const { name } = community;
   const { address } = user;
 
+  // Render Error
+  const renderError = (error) => <p className={styles.error}>{error}</p>;
+
+  // Validations
+  const validateEmail = (value) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return !re.test(String(value).toLowerCase()) ? 'Invalid Email' : undefined;
+  };
+
+  const validateDescription = (value) => {
+    if (value === undefined || value.length <= 0) {
+      return 'Must include a description';
+    }
+    if (value && value.length > 280) {
+      return 'Must use less than 280 characters';
+    }
+    return undefined;
+  };
+
+  // character count
+  const renderCharacterCount = (value) => {
+    if (!value) {
+      return '280/0 Characters';
+    }
+    return '280/' + value.length + ' Characters';
+  };
+
   return (
-    <form className={styles.form}>
-      <h1>Join {name} Community</h1>
-      <label>Please Enter your email</label>
-      <input type="text" />
+    <Form
+      onSubmit={(values) => {
+        const { description, email } = values;
+        console.log('description', description);
+        console.log('email', email);
 
-      <label>Wallet Address</label>
-      <p className={styles.address}>{address}</p>
+        // call action
+      }}
+    >
+      {({ handleSubmit, pristine, invalid }) => (
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <h1>Join {name} Community</h1>
 
-      <div className={styles.descriptionContainer}>
-        <p>
-          Please provide a short message describing why you would like to join
-          our community.
-        </p>
-        <textarea />
-        <span>280 Characters Max</span>
-      </div>
+          <Field name="email" validate={validateEmail}>
+            {({ input, meta }) => (
+              <Fragment>
+                <label>Please Enter your email</label>
+                <input type="text" {...input} autoFocus />
+                {meta.error && meta.touched && renderError(meta.error)}
+              </Fragment>
+            )}
+          </Field>
 
-      <div className={styles.buttonContainer}>
-        <Button theme="primary" content="Request to Join Community" />
-        <span>Cancel</span>
-      </div>
-    </form>
+          <label>Wallet Address</label>
+          <p className={styles.address}>{address}</p>
+
+          <Field name="description" validate={validateDescription}>
+            {({ input, meta }) => (
+              <div className={styles.descriptionContainer}>
+                <p>
+                  Please provide a short message describing why you would like
+                  to join our community.
+                </p>
+                <textarea {...input} />
+                <span>{renderCharacterCount(input.value)}</span>
+                {meta.error && meta.touched && renderError(meta.error)}
+              </div>
+            )}
+          </Field>
+
+          <div className={styles.buttonContainer}>
+            <Button
+              theme="primary"
+              content="Request to Join Community"
+              type="submit"
+              disabled={pristine || invalid}
+            />
+            <span>Cancel</span>
+          </div>
+        </form>
+      )}
+    </Form>
   );
 };
 
 export default CommunityPrivateUserRequest;
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     updateUser: bindActionCreators(updateUser, dispatch),
+//   };
+// };
+
+// export default connect(
+//   null,
+//   mapDispatchToProps,
+// )(EditProfile);
