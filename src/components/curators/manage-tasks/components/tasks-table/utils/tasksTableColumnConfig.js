eb69a0c5eec56d7c70950e49042b7dfe73d2ec63
ChrisPlease <chris@chrisplease.me>
2018-12-04 16:03:55 -0700

    update table with actions

--- src/components/curators/manage-tasks/components/tasks-table/utils/tasksTableColumnConfig.js
+++ src/components/curators/manage-tasks/components/tasks-table/utils/tasksTableColumnConfig.js
@@ -1,12 +1,30 @@
+import React, { Fragment } from 'react';
 import moment from 'moment';
+
+import Filter from '../../../../../shared/filter';
+import Button from '../../../../../shared/button';
+import { capitalizeFirstLetter } from '../../../../../../utils/helpers';
+
+const applyFilter = (arr, filterStatus) =>
+  (arr || []).filter((i) => i.status === filterStatus);
+
+const statuses = ['initialized', 'escrowed', 'claimed'];
+
+const filters = [
+  {
+    name: 'All',
+    filter: (items) => items,
+  },
+  ...statuses.map((status) => ({
+    name: capitalizeFirstLetter(status),
+    filter: (items) => applyFilter(items, status),
+  })),
+];
+
 export const tasksTableColumnConfig = [
   {
     Header: 'Task Title',
     accessor: 'title',
-  },
-  {
-    Header: 'Description',
-    accessor: 'description',
   },
   {
     Header: 'Time to Complete',
@@ -30,9 +48,40 @@ export const tasksTableColumnConfig = [
   {
     Header: 'Status',
     accessor: 'status',
+    filterable: true,
+    filterAll: true,
+    headerStyle: {
+      overflow: 'visible',
+    },
+    filterMethod: ({ value: filter }, rows) => filter(rows),
+    Cell: ({ value }) => capitalizeFirstLetter(value),
+    Filter: ({ onChange }) => (
+      <Filter
+        filters={filters}
+        activeFilter={filters[0]}
+        selectHandler={({ filter }) => onChange(filter)}
+      />
+    ),
   },
   {
     Header: 'Actions',
     accessor: 'actions',
+    Cell: ({ row }) => {
+      const { status } = row;
+      switch (status) {
+        case 'claimed':
+          return (
+            <Fragment>
+              <Button block theme="secondary" content="Approve" />
+              <br />
+              <Button block theme="primary" content="Deny" />
+            </Fragment>
+          );
+        case 'escrowed':
+          return <Button theme="primary" content="Cancel" />;
+        default:
+          return;
+      }
+    },
   },
 ];
